9a639c62e46d02d8cdf0d1e453b8939fc9b6ccc8
profitsw2000 <profitsw2000@gmail.com>
2022-01-15 22:26:52 +0300

    Создан интерфейс репозитория с функциями извлечения данных о фильме и его реализация(Repository.kt, RepositoryImpl.kt).

---
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/model/repository/RepositoryImpl.kt
@@ -0,0 +1,17 @@
+package ru.profitsw2000.moviecollectiondb.model.repository
+
+import ru.profitsw2000.moviecollectiondb.model.representation.Movie
+
+/**
+ *
+ */
+class RepositoryImpl : Repository {
+    override fun getMovieFromServer(): Movie {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMovieFromLocalStorage(): Movie {
+        TODO("Not yet implemented")
+    }
+
+}
