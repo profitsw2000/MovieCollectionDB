f03af55218650b18bc59d15b5d9b2c80dba26df3
profitsw2000 <profitsw2000@gmail.com>
2022-01-16 14:54:18 +0300

    Внесены значения по умолчанию для объекта класса Movie.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/model/representation/Movie.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/model/representation/Movie.kt
@@ -1,15 +1,24 @@
 package ru.profitsw2000.moviecollectiondb.model.representation
 
+import ru.profitsw2000.moviecollectiondb.R
 import java.util.*
 
 data class Movie(
-    val title: String,
-    val genre: String,
-    val duration: Int,
-    val rating: Float,
-    val budget: Int,
-    val revenue: Int,
-    val releaseDate: String,
-    val description: String,
-    val picture: Int
+    val title: String = "Никто",
+    val genre: String = "боевик, триллер",
+    val duration: Int = 92,
+    val rating: Float = 7.3f ,
+    val budget: Int = 16000000,
+    val revenue: Int = 55405035,
+    val releaseDate: String = "18 марта 2021",
+    val description: String = "Непримечательный и незаметный семьянин Хатч " +
+            "живёт скучной жизнью обычного аудитора, пока однажды в его дом не " +
+            "вламываются грабители. И это бы сошло им с рук, если бы они не " +
+            "забрали браслетик его маленькой дочки. Не в силах это терпеть, " +
+            "Хатч отправляется на поиски наглецов, а на обратном пути ввязывается " +
+            "в драку с пьяными хулиганами, пристававшими к девушке в общественном " +
+            "транспорте. От души помахав кулаками, наш аудитор отправляет дебоширов" +
+            " в больницу, но оказывается, что один из пострадавших — брат " +
+            "влиятельного русского бандита. И он теперь жаждет мести.",
+    val picture: Int = R.drawable.film
 )
--- app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/main/MainViewModel.kt
@@ -1,8 +1,31 @@
 package ru.profitsw2000.moviecollectiondb.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.profitsw2000.moviecollectiondb.model.AppState
+import ru.profitsw2000.moviecollectiondb.model.repository.Repository
+import java.lang.Thread.sleep
 
-class MainViewModel : ViewModel() {
+class MainViewModel (private val repository: Repository) : ViewModel() {
+    private val liveData = MutableLiveData<AppState>()
+
+    fun getLiveData(): LiveData<AppState> = liveData
+
+    fun getMovie() = getDataFromLocalSource()
+
+    private fun getDataFromLocalSource() {
+        val randomNumber = (0..5).random()
+
+        liveData.value = AppState.Loading
+        Thread {
+            sleep(3000)
+            if (randomNumber < 4) {
+                liveData.postValue(AppState.Success(repository.getMovieFromLocalStorage()))
+            } else {
+            }
+        }.start()
+    }
+
 
 }
