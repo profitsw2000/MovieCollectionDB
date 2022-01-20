adaa6b86f519d6fc59a319cc673c09e8a59061e3
profitsw2000 <profitsw2000@gmail.com>
2022-01-20 06:58:10 +0300

    Удаление кода в ParentAdapter.kt, ставшего не действующим в связи с изменениями в файле макета.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/adapters/ParentAdapter.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/adapters/ParentAdapter.kt
@@ -6,19 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ru.profitsw2000.moviecollectiondb.databinding.FragmentMainRecyclerItemBinding
+import ru.profitsw2000.moviecollectiondb.model.representation.Category
 import ru.profitsw2000.moviecollectiondb.model.representation.Movie
 import ru.profitsw2000.moviecollectiondb.ui.main.MainFragment
 
-class ParentAdapter (private val itemClickListener: MainFragment.OnItemViewClickListener)
+class ParentAdapter (private val categories: List<Category>, private val itemClickListener: MainFragment.OnItemViewClickListener)
     : RecyclerView.Adapter<ParentAdapter.MainViewHolder>() {
 
     private val viewPool = RecyclerView.RecycledViewPool()
-    private var moviesData: List<Movie> = listOf()
+    private var categoriesData: List<Category> = listOf()
     private lateinit var binding: FragmentMainRecyclerItemBinding
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setMovie(data: List<Movie>) {
-        moviesData = data
+    fun setCategories(data: List<Category>) {
+        categoriesData = data
         notifyDataSetChanged()
     }
 
@@ -30,16 +31,16 @@ class ParentAdapter (private val itemClickListener: MainFragment.OnItemViewClick
     }
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-        holder.bind(moviesData[position])
+        holder.bind(categories[position])
     }
 
-    override fun getItemCount() = moviesData.size
+    override fun getItemCount() = categories.size
 
     inner class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(movie: Movie) = with(binding) {
-            mainFragmentRecyclerItemTextView.text = movie.title
+        fun bind(category: Category) = with(binding) {
+/*            mainFragmentRecyclerItemTextView.text = movie.title
             movieDescriptionImage.setImageResource(movie.picture)
-            root.setOnClickListener { itemClickListener.onItemViewClick(movie) }
+            root.setOnClickListener { itemClickListener.onItemViewClick(movie) }*/
         }
     }
 }
