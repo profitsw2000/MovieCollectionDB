30927cca47027747afea9715671df185451186ab
profitsw2000 <profitsw2000@gmail.com>
2022-01-25 17:21:37 +0300

    Реализована функция getMoviesFromServer для получения списка фильмов с сервера с кратким описание по выбранному жанру. Данная функция применяется для заполнения экземпляра Categories (поле movieList).

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/model/repository/RepositoryImpl.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/model/repository/RepositoryImpl.kt
@@ -3,7 +3,9 @@ package ru.profitsw2000.moviecollectiondb.model.repository
 import ru.profitsw2000.moviecollectiondb.model.Factory.CategoriesFactory.getCategories
 import ru.profitsw2000.moviecollectiondb.model.Factory.MoviesFactory.getMovies
 import ru.profitsw2000.moviecollectiondb.model.GenresLoader
+import ru.profitsw2000.moviecollectiondb.model.TitlesLoader
 import ru.profitsw2000.moviecollectiondb.model.representation.Category
+import ru.profitsw2000.moviecollectiondb.model.representation.Movie
 
 /**
  *
@@ -11,7 +13,28 @@ import ru.profitsw2000.moviecollectiondb.model.representation.Category
 class RepositoryImpl : Repository {
     val API_KEY = "c653b216d7d09c4aa4176e651f1ac4dd"
 
+    override fun getMoviesFromServer(request: String): List<Movie> {
+
+        var movies: ArrayList<Movie> = arrayListOf()
+        var dto = TitlesLoader.loadTitles(request)
+
+        var i = dto?.results?.size
+
+        if (dto != null) {
+            if (i != null) {
+                var j = 0
+                while (i > 0) {
+                    movies.add(Movie(title = dto.results?.get(j)?.title ?: "Неизвестно"))
+                    i--
+                    j++
+                }
+            }
+        }
+        return movies
+    }
+
     override fun getCategoriesFromServer(request: String): List<Category> {
+        val requestMovieList = "api_key=c653b216d7d09c4aa4176e651f1ac4dd&language=ru&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres="
         var categories: ArrayList<Category> = arrayListOf()
         val dto = GenresLoader.loadGenres(request)
 
@@ -21,9 +44,22 @@ class RepositoryImpl : Repository {
             if (i != null) {
                 var j = 0
                 while (i > 0) {
-                    categories.add(Category(
-                        dto.genres?.get(j)?.id ?: 0,dto.genres?.get(j)?.name ?: "Неизвестно",
-                        getMovies(20)))
+                    val id = dto.genres?.get(j)?.id ?: 0
+                    val moviesList = getMoviesFromServer(requestMovieList + id.toString())
+                    if (moviesList != null) {
+                        categories.add(Category(
+                            dto.genres?.get(j)?.id ?: 0,dto.genres?.get(j)?.name ?: "Неизвестно",
+                            moviesList))
+                    }
+                    else {
+                        categories.add(
+                            Category(
+                                dto.genres?.get(j)?.id ?: 0,
+                                dto.genres?.get(j)?.name ?: "Неизвестно",
+                                getMovies(20)
+                            )
+                        )
+                    }
                     i--
                     j++
                 }
