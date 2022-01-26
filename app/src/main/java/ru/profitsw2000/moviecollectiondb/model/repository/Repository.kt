c0a3d05376dd298add13dd254f33fe4cc82acd12
profitsw2000 <profitsw2000@gmail.com>
2022-01-26 08:56:26 +0300

    Интерфейс Repository.kt  - добавлена функция для запроса данных с сервера с данными о конкретном фильме.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/model/repository/Repository.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/model/repository/Repository.kt
@@ -4,7 +4,7 @@ import ru.profitsw2000.moviecollectiondb.model.representation.Category
 import ru.profitsw2000.moviecollectiondb.model.representation.Movie
 
 interface Repository {
-//    fun getMovieFromServer(id: Int): Movie
+    fun getMovieFromServer(id: Int): Movie
     fun getMoviesFromServer(request: String): List<Movie>
     fun getCategoriesFromServer(request: String): List<Category>
     fun getMoviesFromLocalStorage(): List<Movie>
