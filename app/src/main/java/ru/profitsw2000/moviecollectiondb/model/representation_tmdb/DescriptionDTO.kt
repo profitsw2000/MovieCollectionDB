d9281d34dfe767da9d1291ceddf087ad150606ba
profitsw2000 <profitsw2000@gmail.com>
2022-01-26 10:11:26 +0300

    В проект добавлены классы для считывания данных с сервера - DescriptionDTO.kt и GroupDTO.kt.

---
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/model/representation_tmdb/DescriptionDTO.kt
@@ -0,0 +1,15 @@
+package ru.profitsw2000.moviecollectiondb.model.representation_tmdb
+
+import java.time.Duration
+
+data class DescriptionDTO(
+    val budget: Int?,
+    val genres: List<GroupDTO>?,
+    val id: Int?,
+    val overview: String?,
+    val release_date: String?,
+    val revenue: Int?,
+    val runtime: Int?,
+    val title: String?,
+    val vote_average: Int?
+)
