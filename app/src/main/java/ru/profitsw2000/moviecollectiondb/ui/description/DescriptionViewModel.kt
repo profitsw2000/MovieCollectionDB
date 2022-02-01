8f2cfb6a67c021a65181d834b6eecfbb7e11bcd3
profitsw2000 <profitsw2000@gmail.com>
2022-02-01 14:28:02 +0300

    Внесены изменения в реализацию DescriptionViewModel.kt для работы с Retrofit. Добавлен пакэдж Utils с функцией конвертирования данных с типа DescriptionDTO в Movie.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/description/DescriptionViewModel.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/description/DescriptionViewModel.kt
@@ -3,10 +3,22 @@ package ru.profitsw2000.moviecollectiondb.ui.description
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import ru.profitsw2000.moviecollectiondb.model.AppState
-import ru.profitsw2000.moviecollectiondb.model.repository.Repository
+import ru.profitsw2000.moviecollectiondb.model.repository.DescriptionRepository
+import ru.profitsw2000.moviecollectiondb.model.repository.DescriptionRepositoryImpl
+import ru.profitsw2000.moviecollectiondb.model.representation_tmdb.DescriptionDTO
+import ru.profitsw2000.moviecollectiondb.retrofit.RemoteDataSource
+import ru.profitsw2000.moviecollectiondb.utils.convertDescriptionDTOToModel
 
-class DescriptionViewModel(private val repository: Repository) : ViewModel() {
+private const val SERVER_ERROR = "Ошибка сервера"
+private const val REQUEST_ERROR = "Ошибка запроса на сервер"
+private const val CORRUPTED_DATA = "Неполные данные"
+
+class DescriptionViewModel(private val descriptionRepositoryImpl: DescriptionRepository =
+    DescriptionRepositoryImpl(RemoteDataSource())) : ViewModel() {
     private val localLiveData: MutableLiveData<AppState> = MutableLiveData()
     val movieLiveData: LiveData<AppState> get() {
         return localLiveData
@@ -16,9 +28,43 @@ class DescriptionViewModel(private val repository: Repository) : ViewModel() {
 
     private fun loadData(id: Int) {
         localLiveData.value = AppState.Loading
-        Thread{
-            val data = repository.getMovieFromServer(id)
-            localLiveData.postValue(AppState.MovieSuccess(data))
-        }.start()
+        descriptionRepositoryImpl.getMovieDescriptionFromServer(id, callBack)
+        //Thread{
+            //val data = repository.getMovieFromServer(id)
+            //localLiveData.postValue(AppState.MovieSuccess(data))
+        //}.start()
+    }
+
+    private val callBack = object :
+        Callback<DescriptionDTO> {
+        override fun onResponse(call: Call<DescriptionDTO>, response:
+        Response<DescriptionDTO>
+        ) {
+            val serverResponse: DescriptionDTO? = response.body()
+            localLiveData.postValue(
+                if (response.isSuccessful && serverResponse != null) {
+                    checkResponse(serverResponse)
+                } else {
+                    AppState.Error(SERVER_ERROR)
+                }
+            )
+        }
+
+        override fun onFailure(call: Call<DescriptionDTO>, t: Throwable) {
+            localLiveData.postValue(AppState.Error(REQUEST_ERROR))
+        }
+
+        private fun checkResponse(serverResponse: DescriptionDTO): AppState {
+            with(serverResponse){
+                return if (budget == null || genres == null || id == null ||
+                    overview.isNullOrEmpty() || release_date.isNullOrEmpty() ||
+                        revenue == null || runtime == null || title.isNullOrEmpty() ||
+                        vote_average == null) {
+                    AppState.Error(CORRUPTED_DATA)
+                } else {
+                    AppState.MovieSuccess(convertDescriptionDTOToModel(serverResponse))
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/utils/DataUtils.kt
@@ -0,0 +1,28 @@
+package ru.profitsw2000.moviecollectiondb.utils
+
+import ru.profitsw2000.moviecollectiondb.R
+import ru.profitsw2000.moviecollectiondb.model.representation.Movie
+import ru.profitsw2000.moviecollectiondb.model.representation_tmdb.DescriptionDTO
+
+fun convertDescriptionDTOToModel(descriptionDTO: DescriptionDTO) : Movie {
+
+    var genresList: ArrayList<String> = arrayListOf()
+    var i = descriptionDTO.genres?.size
+
+    //get string with genres of movie from List of strings
+    if (i != null) {
+        var j = 0
+        while (i > 0) {
+            genresList.add(descriptionDTO.genres?.get(j)?.name ?: "undefined")
+            i--
+            j++
+        }
+    }
+    val genresString = genresList.joinToString(separator = ", ")
+
+    with(descriptionDTO){
+        return Movie(title!!, genresString, runtime!!, vote_average!!,
+            budget!!, revenue!!, release_date!!, overview!!, R.drawable.film,
+            id!!)
+    }
+}
