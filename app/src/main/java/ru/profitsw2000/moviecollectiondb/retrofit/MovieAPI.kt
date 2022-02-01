5a99a059d48e901ccfd7c9f5ee9cc1365240ff7f
profitsw2000 <profitsw2000@gmail.com>
2022-02-01 09:38:43 +0300

    Создан интерфейс с описанием запроса на сервер.

---
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/retrofit/MovieAPI.kt
@@ -0,0 +1,17 @@
+package ru.profitsw2000.moviecollectiondb.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+import ru.profitsw2000.moviecollectiondb.model.representation_tmdb.DescriptionDTO
+
+interface MovieAPI {
+    @GET("/movie/{movie_id}")
+    fun getMovie(
+        @Path("movie_id") id: Int,
+        @Query("api_key") api_key: String,
+        @Query("language") language: Double
+    ): Call<DescriptionDTO>
+}
