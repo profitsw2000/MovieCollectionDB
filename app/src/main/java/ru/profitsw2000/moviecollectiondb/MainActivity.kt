b82623cb108b14526337a4093571972dfadedf07
profitsw2000 <profitsw2000@gmail.com>
2022-02-02 14:12:52 +0300

    Создана папка для ресурсов меню и макет для бокового меню. Добавлены элементы бокового меню.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.profitsw2000.moviecollectiondb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import ru.profitsw2000.moviecollectiondb.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,29 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_settings-> {
+                true
+            }
+
+            R.id.menu_favorite-> {
+                true
+            }
+
+            R.id.menu_history-> {
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
