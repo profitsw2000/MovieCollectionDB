4bbd3dd0930588c52a2cf4d46514fd539255be5c
profitsw2000 <profitsw2000@gmail.com>
2022-02-02 14:38:06 +0300

    В MainActivity.kt добавлена функция для запуска фрагмента. При вызове этой функции будет происходить запуск фрагмента, переданного в параметрах.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/MainActivity.kt
@@ -1,9 +1,11 @@
 package ru.profitsw2000.moviecollectiondb
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import ru.profitsw2000.moviecollectiondb.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu,menu)
         return super.onCreateOptionsMenu(menu)
@@ -26,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             R.id.menu_settings-> {
+
                 true
             }
 
@@ -38,6 +42,15 @@ class MainActivity : AppCompatActivity() {
             }
 
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.apply {
+            beginTransaction()
+                .add(R.id.container, fragment)
+                .addToBackStack("")
+                .commitAllowingStateLoss()
         }
     }
 }
