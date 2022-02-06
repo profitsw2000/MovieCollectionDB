fcd10076dc12cdda211c6f3acbb3472618a59af0
profitsw2000 <profitsw2000@gmail.com>
2022-02-06 21:17:00 +0300

    Создана модель для передачи данных во фрагмент со списком фильмов, добавленных в избранное. Модель прописана в KOIN-модуле.

--- app/src/main/java/ru/profitsw2000/moviecollectiondb/KoinModule.kt
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/KoinModule.kt
@@ -6,6 +6,7 @@ import ru.profitsw2000.moviecollectiondb.model.repository.*
 import ru.profitsw2000.moviecollectiondb.retrofit.RemoteDataSource
 import ru.profitsw2000.moviecollectiondb.room.NoteDAO
 import ru.profitsw2000.moviecollectiondb.ui.description.DescriptionViewModel
+import ru.profitsw2000.moviecollectiondb.ui.favorite.FavoriteViewModel
 import ru.profitsw2000.moviecollectiondb.ui.main.MainViewModel
 import ru.profitsw2000.moviecollectiondb.ui.notes.NoteViewModel
 
@@ -13,9 +14,11 @@ val appModule = module {
     single<Repository> {RepositoryImpl()}
     single<DescriptionRepository> {DescriptionRepositoryImpl(RemoteDataSource())}
     single<LocalRepository> {LocalRepositoryImpl(App.getNoteDao())}
+    single<LocalFavRepository> {LocalFavRepositoryImpl(App.getFavoriteDao())}
 
     //View models
     viewModel { MainViewModel(get()) }
     viewModel { DescriptionViewModel(get()) }
     viewModel { NoteViewModel(get()) }
+    viewModel { FavoriteViewModel(get()) }
 }
