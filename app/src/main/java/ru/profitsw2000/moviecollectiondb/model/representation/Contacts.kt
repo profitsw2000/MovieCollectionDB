d54a6b82699f142e5396978436d2175c4db96f17
profitsw2000 <profitsw2000@gmail.com>
2022-02-08 18:24:16 +0300

    Адаптер для списка контактов.

---
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/model/representation/Contacts.kt
@@ -0,0 +1,6 @@
+package ru.profitsw2000.moviecollectiondb.model.representation
+
+data class Contacts(
+    val name: String,
+    val telephone_number: String
+)
---
+++ app/src/main/java/ru/profitsw2000/moviecollectiondb/ui/adapters/ContactsAdapter.kt
@@ -0,0 +1,46 @@
+package ru.profitsw2000.moviecollectiondb.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.profitsw2000.moviecollectiondb.databinding.FragmentFavoriteRecyclerItemBinding
+import ru.profitsw2000.moviecollectiondb.model.representation.Contacts
+import ru.profitsw2000.moviecollectiondb.room.FavoriteEntity
+
+class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+
+    private var data: List<Contacts> = arrayListOf()
+    private lateinit var binding: FragmentFavoriteRecyclerItemBinding
+
+    fun setData(data: List<Contacts>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = FragmentFavoriteRecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)
+    {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(contacts: Contacts) {
+            with(binding){
+                //movieTitle.text = favorite.title
+            }
+        }
+    }
+}
